626fbd9461dc4b977e5ac06cee87d4d84601998a
MatteoMorisco <moriscomatteo@gmail.com>
2021-10-11 14:35:27 +0200

    Fix 2

--- src/main/web/static/js/prenotazioni.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function (Event) {
-    let urlGetReparti = 'http://localhost:8080/api/get-reparti';
-    let urlGetMediciByIdReparto = 'http://localhost:8080/api/get-medici-by-reparto/{id}';
-    let urlGetOrariByMediciDisponibili = 'http://localhost:8080/api/get-reparti';
-
-})
