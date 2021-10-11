b9e2ed050276011cee5877781da818f7c8a635c8
Nicolò <58290931+fnikoro@users.noreply.github.com>
2021-10-11 18:02:01 +0200

    Add files via upload

--- src/main/web/static/js/prenotazioni.js
+++ src/main/web/static/js/prenotazioni.js
@@ -1,87 +1,102 @@
+let form = document.querySelector('#preForm')
+let fields = document.querySelectorAll(".required")
+let selectReparto = document.getElementById("id_reparto")
+let selectMedico = document.getElementById("id_medico")
+let primoId = document.getElementById("primo")
+let secondoId = document.getElementById("secondo")
+let scala = document.querySelector('#data_prenotazione')
+
 window.addEventListener('load', function (event) {
-    let form = document.querySelector('#preForm')
-    let fields = document.querySelectorAll(".required")
-    let selectReparto = document.getElementById("id_reparto")
-    let selectMedico = document.getElementById("id_medico")
-    let primoId = document.getElementById("primo")
-    let secondoId = document.getElementById("secondo")
+    selectReparto.addEventListener("change", mediciSelect);
+    selectMedico.addEventListener("change", orariSelect);
+    event.preventDefault();
+})
 
-    selectReparto.addEventListener("change", function(event) {
-        let urlGetMediciByIdReparto = 'http://localhost:8080/api/get-medici-by-reparto/1';
-        // let urlGetOrariByMediciDisponibili = 'http://localhost:8080/api/';
+form.addEventListener('submit', function (event) {
+    let formValidity = true;
+    event.preventDefault();
 
-        fetch(urlGetMediciByIdReparto).then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            let dat = Object.values(data)
-            console.log(dat)
+    fields.forEach(function (el, i, ar) {
+        if (el.value === 0) {
+            console.log("Trovato campo vuoto: " + el.name)
+            let formRow = el.closest(".form-row")
+            let messaggi = formRow.querySelector(".msg")
+            messaggi.classList.remove("d-none")
 
-            /*var result = [];
-
-            for(var i in data)
-                result.push([i, data [i]]);
-
-
-            var dat = new google.visualization.DataTable();
-            dat.addColumn('string', 'Topping');
-            dat.addColumn('number', 'Slices');
-            dat.addRows(result);*/
-            primoId.innerHTML = `${dat[0].nome} ${dat[0].cognome}`
-            secondoId.innerHTML = `${dat[1].nome} ${dat[1].cognome}`
-        })
+            formValidity = false;
+        } else {
+            let formRow = el.closest(".form-row")
+            let messaggi = formRow.querySelector(".msg")
+            messaggi.classList.add("d-none")
+        }
     })
 
-    /*form.addEventListener('submit', function (event) {
-        let formValidity = true;
+    if (formValidity === false) {
+        console.log("Erorri trovati, qualquadra che non rotonda")
+        event.preventDefault()
+    }
 
-        fields.forEach(function (el, i, ar) {
-            if (el.value === "") {
-                console.log("Trovato campo uotvo: " + el.name)
-                let formRow = el.closest(".form-row")
-                let messaggi = formRow.querySelector(".msg")
-                messaggi.classList.remove("d-none")
+    let id_reparto = document.querySelector("#id_reparto");
+    let id_medico = document.querySelector("#id_medico");
+    let data_prenotazione = document.querySelector("#data_prenotazione");
+    var data = JSON.stringify({
+        id_reparto: id_reparto.value,
+        id_medico: id_medico.value,
+        data_prenotazione: data_prenotazione.value
+    });
 
-                formValidity = false;
+    fetch('http://localhost:8080/api/save-visite', {
+        method: 'POST',
+        body: data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(resp => {
+            if (resp.status !== 200) {
+                console.log("Status: " + resp.status)
+                return Promise.reject("server")
             } else {
-                let formRow = el.closest(".form-row")
-                let messaggi = formRow.querySelector(".msg")
-                messaggi.classList.add("d-none")
+                console.log("")
             }
         })
-
-        if (formValidity === false) {
-            console.log("Erorri trovati, qualquadra che non rotonda")
-            event.preventDefault()
-        }
-
-        let id_reparto = document.querySelector("#id_reparto");
-        let id_medico = document.querySelector("#id_medico");
-        let data_prenotazione = document.querySelector("#data_prenotazione");
-        var data = JSON.stringify({
-            id_reparto: id_reparto.value,
-            id_medico: id_medico.value,
-            data_prenotazione: data_prenotazione.value
-        });
-
-        /*fetch('http://localhost:8080/api/save-pazienti', {
-            method: 'POST',
-            body: data,
-            headers: {
-                "Content-Type": "application/json",
-            },
+        .catch(err => {
+            if (err === "server")
+                console.log("")
         })
-            .then(resp => {
-                if (resp.status !== 200) {
-                    console.log("Status: " + resp.status)
-                    return Promise.reject("server")
-                } else {
-                    console.log("")
-                }
-            })
-            .catch(err => {
-                if (err === "server")
-                    console.log("")
-            })
-        event.preventDefault()
-    })*/
+    event.preventDefault()
 })
+
+function mediciSelect() {
+    let repartoId = document.getElementById("id_reparto").value;
+    let urlGetMediciByIdReparto = 'http://localhost:8080/api/get-medici-by-reparto/' + repartoId;
+
+    fetch(urlGetMediciByIdReparto).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        let dat = Object.values(data)
+        console.log(dat)
+        primoId.innerHTML = `${dat[0].nome} ${dat[0].cognome}`
+        secondoId.innerHTML = `${dat[1].nome} ${dat[1].cognome}`
+        primoId.value = `${dat[0].id_medico}`;
+        secondoId.value = `${dat[1].id_medico}`;
+    })
+}
+
+function orariSelect() {
+    let medicoId = document.getElementById("id_medico").value;
+    let urlGetListaOrariMedico = 'http://localhost:8080/api/get-lista-orari-medico/' + medicoId;
+
+    fetch(urlGetListaOrariMedico).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        let dat = Object.values(data)
+        console.log(dat)
+
+        for(let i = 0; i < dat.length; i++) {
+            let optionX = document.createElement("option")
+            optionX.innerHTML = `${dat[i]}`
+            scala.appendChild(optionX)
+        }
+    })
+}
