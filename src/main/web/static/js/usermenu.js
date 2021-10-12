f281305cb589d892a04522baa30d1f744e18932a
Nicolò <58290931+fnikoro@users.noreply.github.com>
2021-10-12 19:16:24 +0200

    Delete src/main/web directory

--- src/main/web/static/js/usermenu.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function(e){
-    document.querySelector('.userMenu').addEventListener('click', function (e){
-        e.preventDefault()
-        document.querySelector("#loggedUserMenu").classList.toggle("open")
-    })
-})
